b7cfee27485b01e44110ffeba31c4a5231f76712
Balaji-CDE <balaji.p3@cognizant.com>
2023-11-23 17:15:07 +0530

    Additional changes for allignment

--- src/components/dora/DoraMetrics.js
+++ src/components/dora/DoraMetrics.js
@@ -2,48 +2,54 @@ import React from 'react'
 import './DoraMetrics.css'
 import ChartjsOne from './ChartjsOne'
 
+const data1 = "02";
+const data2 = "23.2";
+const data3 = "02";
+const data4 = "7";
+
+
 function DoraMetrics() {
     return (
         <div className="w-75 p-3 background">
             <div className="card background2">
-                <h2 className='text-start mx-3 title'>General / DORA Metrics</h2>
+                <h2 className='text-start mx-3 title pt-3'>General / DORA Metrics</h2>
                 <div className="row p-3">
                     <div className="col-sm-12 mb-3 mb-sm-0">
                         <div className="row">
                             <div className="col-sm-3 mb-3 mb-sm-0">
-                                <div className="card text-center">
+                                <div className="card text-center h-100 pb-2">
                                     <div className="card-body">
                                         <span className="card-desc">Deployment frequency per month{' '}</span>
-                                        <p className="card-text text-wrap">
-                                            <span className='card-1-val'>02</span></p>
                                     </div>
+                                    <div className="card-text text-wrap card-1-val">
+                                        {data1}</div>
                                 </div>
                             </div>
                             <div className="col-sm-3 mb-3 mb-sm-0">
-                                <div className="card text-center">
+                                <div className="card text-center h-100 pb-2">
                                     <div className="card-body">
                                         <span className="card-desc">Lead Time for changes{' '}</span>
-                                        <p className="card-text text-wrap">
-                                            <span className='card-2-val'>23.2 <span className='subscript'>hours</span></span></p>
                                     </div>
+                                    <div className="card-text text-wrap card-2-val">
+                                        {data2} <span className='subscript'>hours</span></div>
                                 </div>
                             </div>
                             <div className="col-sm-3 mb-3 mb-sm-0">
-                                <div className="card text-center">
+                                <div className="card text-center h-100 pb-2">
                                     <div className="card-body">
                                         <span className="card-desc">Mean Time to Recover{' '}</span>
-                                        <p className="card-text text-wrap">
-                                            <span className='card-3-val'>02 <span className='subscript'>hours</span></span></p>
                                     </div>
+                                    <div className="card-text text-wrap card-3-val">
+                                        {data3}{' '}<span className='subscript'>hours</span></div>
                                 </div>
                             </div>
                             <div className="col-sm-3 mb-3 mb-sm-0">
-                                <div className="card text-center">
+                                <div className="card text-center h-100 pb-2">
                                     <div className="card-body">
                                         <span className="card-desc">Change Failure Rate{' '}</span>
-                                        <p className="card-text text-wrap">
-                                            <span className='card-4-val' id='card-4'>7 </span> <span className='card-4-val' id='percent'>%</span></p>
                                     </div>
+                                    <div className="card-text text-wrap card-4-val" id='card-4'>
+                                        {data4} <span className='card-4-val' id='percent'>%</span></div>
                                 </div>
                             </div>
                         </div>
@@ -53,9 +59,9 @@ function DoraMetrics() {
                 <div className="row p-3 mb-5">
                     <div className="col-sm-6 mb-4 mb-sm-0">
                         <div className='card'>
-                        <ChartjsOne />
+                            <ChartjsOne />
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
