ff49ed53f6ed374642bfd903339276da805373a5
VaseemFathima-36 <MVaseem.Fathima@cognizant.com>
2023-11-23 10:58:26 +0530

    added resilience hub page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ResilienceHub from './components/ResilienceHub'
+
+
+function App() {
+  return (
+    <div className="App">
+    <ResilienceHub />
+    </div>
+  );
+}
+
+export default App;
